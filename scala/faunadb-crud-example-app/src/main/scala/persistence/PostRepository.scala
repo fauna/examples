422c79009092aa9943e42fba8ffb5e1c388ee18b
lregnier <leonardoregnier@gmail.com>
2019-02-20 16:09:15 -0300

    Adding Pagination support

--- scala/faunadb-crud-example-app/src/main/scala/persistence/PostRepository.scala
+++ scala/faunadb-crud-example-app/src/main/scala/persistence/PostRepository.scala
@@ -3,7 +3,7 @@ package persistence
 import faunadb.FaunaClient
 import faunadb.query.{Map, _}
 import faunadb.values.{Codec, Value}
-import model.Post
+import model.{Page, PaginationOptions, Post}
 
 import scala.concurrent.Future
 
@@ -15,19 +15,20 @@ class PostRepository(faunaClient: FaunaClient) extends FaunaRepository[Post] {
 
   //-- Custom repository operations specific to the current entity go below --//
 
-  def findByTitle(title: String): Future[Seq[Post]] = {
+  def findByTitle(title: String)(implicit po: PaginationOptions): Future[Page[Post]] = {
+    val beforeCursor = po.before.map(id => Before(Ref(Class(className), id)))
+    val afterCursor = po.after.map(id => After(Ref(Class(className), id)))
+    val cursor = beforeCursor.orElse(afterCursor).getOrElse(NoCursor)
+
     val result: Future[Value] =
       client.query(
-        SelectAll(
-          "data",
-          Map(
-            Paginate(Match(Index("posts_by_title"), title)),
-            Lambda(nextRef => Select("data", Get(nextRef)))
-          )
+        Map(
+          Paginate(Match(Index("posts_by_title"), title), size = po.size, cursor = cursor),
+          Lambda(nextRef => Select("data", Get(nextRef)))
         )
       )
 
-    result.decode[Seq[Post]]
+    result.decode[Page[Post]]
 
   }
 
