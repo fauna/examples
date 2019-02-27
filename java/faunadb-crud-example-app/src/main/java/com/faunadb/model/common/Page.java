bd996009ef9a105232b3a946de0bcbe922ee224d
lregnier <leonardoregnier@gmail.com>
2019-02-27 17:46:44 -0300

    Adding missing JavaDocs to Page and PageOptions classes.

--- java/faunadb-crud-example-app/src/main/java/com/faunadb/model/common/Page.java
+++ java/faunadb-crud-example-app/src/main/java/com/faunadb/model/common/Page.java
@@ -4,22 +4,38 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * It represents a sequence of elements along with
- * cursors indicators to move backwards and forwards.
+ * <p>It represents a sequence of elements along with
+ * cursors indicators to move backwards and forwards.</p>
+ *
+ * <p>Note that this class does not mean to model Fauna's Page
+ * object, but to be a database agnostic page abstraction.</p>
+ *
+ * <p>One of the main differences with Fauna's Page is that the
+ * latter offers a broader flexibility when it comes to the definition
+ * of which elements can be contained within the data and cursors fields.
+ * Those elements, which are defined during the creation of the Index
+ * from which the Page is being derived, has been constrained in this
+ * example in order to keep a simple design. Nonetheless, be aware that
+ * Fauna's Indexes and Pages can be used to build more complex results</p>
+ *
+ * @see <a href="https://app.fauna.com/documentation/reference/queryapi#page">Index</a>
+ * @see <a href="https://app.fauna.com/documentation/reference/queryapi#page">Page</a>
+ * @see <a href="https://app.fauna.com/documentation/reference/queryapi#page">Cursor</a>
  *
  * @param <T> the type of data within the sequence
  */
 public class Page<T> {
+
     private List<T> data;
     private Optional<String> before;
     private Optional<String> after;
 
     /**
-     * It creates a new Page with the given parameters
+     * It creates a new Page with the given parameters.
      *
      * @param data the data within the current sequence
-     * @param before the before cursor – if any, it contains the Id of the previous record before the current sequence
-     * @param after the after cursor – if any, it contains the Id of the next record after the current sequence
+     * @param before the before cursor – if any, it contains the Id of the previous record before the current sequence of data
+     * @param after the after cursor – if any, it contains the Id of the next record after the current sequence of data
      */
     public Page(List<T> data, Optional<String> before, Optional<String> after) {
         this.data = data;
--- java/faunadb-crud-example-app/src/main/java/com/faunadb/model/common/PaginationOptions.java
+++ java/faunadb-crud-example-app/src/main/java/com/faunadb/model/common/PaginationOptions.java
@@ -10,7 +10,13 @@ public class PaginationOptions {
     private Optional<String> before;
     private Optional<String> after;
 
-
+    /**
+     * It creates a new PaginationOptions object with the given parameters.
+     *
+     * @param size the max number of elements to return in the requested Page
+     * @param before the before cursor – if any, it indicates to return the previous Page of results before this Id (exclusive)
+     * @param after the after cursor –  if any, it indicates to return the next Page of results after this Id (inclusive)
+     */
     public PaginationOptions(Optional<Integer> size, Optional<String> before, Optional<String> after) {
         this.size = size;
         this.before = before;
