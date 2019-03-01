d0a93a5ceba85b2457d044fec8ff6d1503ea397a
lregnier <leonardoregnier@gmail.com>
2019-03-01 17:17:21 -0300

    Adding missing ScalaDocs

--- scala/faunadb-crud-example-app/src/main/scala/model/common.scala
+++ scala/faunadb-crud-example-app/src/main/scala/model/common.scala
@@ -13,19 +13,33 @@ trait Entity {
   * It represents a sequence of elements along with
   * cursors indicators to move backwards and forwards.
   *
+  * Note that this class does not mean to model Fauna's Page
+  * object, but to be a database agnostic page abstraction.
+  *
+  * One of the main differences with Fauna's Page is that the
+  * latter offers a broader flexibility when it comes to the definition
+  * of which elements can be contained within the data and cursors fields.
+  * Those elements, which are defined during the creation of the Index
+  * from which the Page is being derived, has been constrained in this
+  * example in order to keep a simple design. Nonetheless, be aware that
+  * Fauna's Indexes and Pages can be used to build more complex results.
+  *
   * @param data the data within the current sequence
-  * @param before the before cursor – if any, it contains the Id of the previous record before the current sequence
-  * @param after the after cursor – if any, it contains the Id of the next record after the current sequence
+  * @param before the before cursor – if any, it contains the Id of the previous record before the current sequence of data
+  * @param after the after cursor – if any, it contains the Id of the next record after the current sequence of data
   * @tparam A the type of data within the sequence
+  *
+  * @see [[https://app.fauna.com/documentation/reference/queryapi#page Index]]
+  * @see [[https://app.fauna.com/documentation/reference/queryapi#page Page]]
+  * @see [[https://app.fauna.com/documentation/reference/queryapi#cursor Cursor]]
   */
 case class Page[A](data: Seq[A], before: Option[String], after: Option[String])
 
 /**
   * It defines parameters for looking up a result using pagination.
   *
-  *
-  * @param size the size of the requested results
-  * @param before the before cursor – if any, it indicates the requested result should
-  * @param after the after cursor – if any, it contains the Id of the next record after the current sequence
+  * @param size the max number of elements to return in the requested Page
+  * @param before the before cursor – if any, it indicates to return the previous Page of results before this Id (exclusive)
+  * @param after the after cursor –  if any, it indicates to return the next Page of results after this Id (inclusive)
   */
 case class PaginationOptions(size: Option[Int], before: Option[String], after: Option[String])
